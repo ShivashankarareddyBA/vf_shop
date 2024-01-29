f8d72175e2b522611f70e43ae37032cb84cf2bd9
shivashankarareddy <ssr2tech@gmail.com>
2024-01-29 15:11:10 +0530

    home page inprogress

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:vf_shop/constants/colors.dart';
+import 'package:vf_shop/constants/image_strings.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +12,22 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 150,
+        decoration: const BoxDecoration(
+          color: grey,
+        ),
+        child: const Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [Image(image: AssetImage(cucumbar))],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
